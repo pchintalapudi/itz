ca969451ac399f31d81c24a459bef7e125ba0838
prem <prem@10.0.0.62>
2017-12-22 15:26:27 -0800

    Minor reorganization

--- src/itzfx/fxml/timing/Clock.java
+++ src/itzfx/fxml/timing/Clock.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package itzfx.fxml;
+package itzfx.fxml.timing;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
