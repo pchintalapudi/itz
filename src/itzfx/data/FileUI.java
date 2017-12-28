d7b6db07c7d44e0a41eb7ef61a2431256994ce9a
prem <prem@10.0.0.62>
2017-12-28 13:14:05 -0800

    Added Documentation to Classes

--- src/itzfx/data/FileUI.java
+++ src/itzfx/data/FileUI.java
@@ -16,71 +16,117 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Window;
 
 /**
+ * This class provides useful UI methods for saving and loading files. The
+ * standard procedure is to prompt the user to save, then open a
+ * {@link FileChooser} to allow the user to choose a file, then use a
+ * corresponding method in {@link Retrieval} to save/open the selected file.
  *
+ * @see Retrieval
  * @author Prem Chintalapudi 5776E
  */
 public class FileUI {
 
     /**
+     * Provides a dialog for saving a {@link Robot} to a file. This method
+     * prompts the user to enter a name for the new robot file before calling
+     * {@link Retrieval#writeRobot(itzfx.Robot, java.io.File)}.
      *
-     * @param r
-     * @param owner
+     * @param r the robot to save
+     * @param owner the window to block
+     *
+     * @see FileUI#save(java.lang.String, java.lang.String, javafx.stage.Window,
+     * java.util.function.Consumer)
      */
     public static void saveRobot(Robot r, Window owner) {
         save("Robot", "*.rbt", owner, f -> Retrieval.writeRobot(r, f));
     }
 
     /**
+     * Provides a dialog for loading a {@link Robot}'s data from a file. This
+     * method prompts the user to select a saved .rbt file before calling
+     * {@link Retrieval#readRobot(itzfx.Robot, java.io.File)}.
      *
-     * @param r
-     * @param owner
+     * @param r the robot to fill with data
+     * @param owner the window to block
+     *
+     * @see FileUI#load(java.lang.String, java.lang.String, javafx.stage.Window,
+     * java.util.function.Consumer)
      */
     public static void fillRobot(Robot r, Window owner) {
         load("Robot", "*.rbt", owner, f -> Retrieval.readRobot(r, f));
     }
 
     /**
+     * Provides a dialog for saving a {@link KeyControl} to a file. This method
+     * prompts the user to enter a name for the new KeyControl file before
+     * calling
+     * {@link Retrieval#writeKeyControl(itzfx.KeyControl, java.io.File)}.
      *
-     * @param kc
-     * @param owner
+     * @param kc the KeyControl to save
+     * @param owner the window to block
+     *
+     * @see FileUI#save(java.lang.String, java.lang.String, javafx.stage.Window,
+     * java.util.function.Consumer)
      */
     public static void saveKeyControl(KeyControl kc, Window owner) {
         save("Controller", "*.kcl", owner, f -> Retrieval.writeKeyControl(kc, f));
     }
 
     /**
+     * Provides a dialog for loading a {@link KeyControl}'s data from a file.
+     * This method prompts the user to select a saved .kcl file before calling
+     * {@link Retrieval#readKeyControl(java.io.File)}.
      *
-     * @param r
-     * @param owner
+     * @param r the robot to load the KeyControl into
+     * @param owner the window to block
+     *
+     * @see FileUI#load(java.lang.String, java.lang.String, javafx.stage.Window,
+     * java.util.function.Consumer)
      */
     public static void getKeyControl(Robot r, Window owner) {
         load("Controller", "*.kcl", owner, f -> r.setController(Retrieval.readKeyControl(f)));
     }
-    
+
     /**
+     * Provides a dialog for saving a rerun to a file. This method prompts the
+     * user to enter a name for the new rerun file before calling
+     * {@link Retrieval#writeRerun(java.util.List, java.io.File)}.
      *
-     * @param r
-     * @param owner
+     * @param r the robot from which to pull a recording
+     * @param owner the window to block
+     *
+     * @see FileUI#save(java.lang.String, java.lang.String, javafx.stage.Window,
+     * java.util.function.Consumer)
      */
     public static void saveRerun(Robot r, Window owner) {
         save("Rerun", "*.rrn", owner, f -> Retrieval.writeRerun(r.saveRecording(), f));
     }
 
     /**
+     * Provides a dialog for loading a rerun's data from a file. This method
+     * prompts the user to select a saved .rrn file before calling
+     * {@link Retrieval#readRerun(itzfx.Robot, java.io.File)}.
      *
-     * @param r
-     * @param owner
+     * @param r the robot to load the rerun into
+     * @param owner the window to block
+     *
+     * @see FileUI#load(java.lang.String, java.lang.String, javafx.stage.Window,
+     * java.util.function.Consumer)
      */
     public static void getRerun(Robot r, Window owner) {
         load("Autonomous", "*.rrn", owner, f -> Retrieval.readRerun(r, f));
     }
 
     /**
+     * The basic method for prompting the user to select a save file. This
+     * method uses a {@link FileChooser} to prompt the user to either create or
+     * pick a file to save into. The file is then passed to the provided
+     * consumer such that actions may be performed on it.
      *
-     * @param descriptor
-     * @param extension
-     * @param owner
-     * @param action
+     * @param descriptor the description of what the file type is
+     * @param extension the extension for the file type
+     * @param owner the window to block
+     * @param action the code that performs actions on the selected file
      */
     public static void save(String descriptor, String extension, Window owner, Consumer<File> action) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "", ButtonType.YES, ButtonType.NO);
@@ -98,11 +144,15 @@ public class FileUI {
     }
 
     /**
+     * The basic method for prompting the user to select a saved file. This
+     * method uses a {@link FileChooser} to prompt the user to either create or
+     * pick a file to load from. The file is then passed to the provided
+     * consumer such that actions may be performed on it.
      *
-     * @param descriptor
-     * @param extension
-     * @param owner
-     * @param action
+     * @param descriptor the description of what the file type is
+     * @param extension the extension for the file type
+     * @param owner the window to block
+     * @param action the code that performs actions on the selected file
      */
     public static void load(String descriptor, String extension, Window owner, Consumer<File> action) {
         FileChooser fc = new FileChooser();
