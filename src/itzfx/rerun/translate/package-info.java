a9421c774777161d24ca800d87c92c57270a87be
prem <prem@10.0.0.62>
2017-12-27 21:53:36 -0800

    Added package documentation to all current packages

---
+++ src/itzfx/rerun/translate/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * This package contains methods to translate saved rerun files into RobotC code.
+ */
+package itzfx.rerun.translate;
