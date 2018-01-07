06e82e6fb90f9ed3c37b2b6a428f005d035bab92
prem <prem@10.0.0.62>
2018-01-06 23:54:01 -0800

    Began Tutorial

---
+++ src/itzfx/tutorial/scenes/Scene2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itzfx.tutorial.scenes;
+
+import itzfx.tutorial.TutorialRobot;
+import itzfx.tutorial.TutorialStep;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author prem
+ */
+public class Scene2 implements TutorialScene {
+
+    @FXML
+    private AnchorPane root;
+
+    private final TutorialRobot tr;
+
+    public Scene2(TutorialRobot tr) {
+        this.tr = tr;
+        tr.setController(TutorialStep.STEP1.getController());
+        root.getChildren().add(tr.getNode());
+    }
+
+    @Override
+    public TutorialScene nextScene() {
+        return null;
+    }
+}
