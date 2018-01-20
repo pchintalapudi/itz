dd43259a88638120c9216777488fa1eb3768fee2
prem <prem@10.0.0.62>
2018-01-20 14:41:18 -0800

    Bug Fixes.

--- src/itzfx/Start.java
+++ src/itzfx/Start.java
@@ -10,8 +10,6 @@ import itzfx.fxml.FXMLController;
 import itzfx.preload.Prestart;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -19,7 +17,6 @@ import java.util.concurrent.locks.LockSupport;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.application.Preloader.ProgressNotification;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -27,7 +24,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import sun.misc.Unsafe;
 
 /**
  *
@@ -38,21 +34,21 @@ public class Start extends Application {
     private FXMLController fxml;
 
     //Because I can
-    public static final Unsafe UNSAFE;
-
-    public static final List<Unsafe> UNSAFES = new LinkedList<>();
-
-    static {
-        try {
-            java.lang.reflect.Field f = LockSupport.class.getDeclaredField("UNSAFE");
-            f.setAccessible(true);
-            UNSAFE = (Unsafe) f.get(null);
-        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
-            Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
-            System.err.println(ex);
-            throw new RuntimeException();
-        }
-    }
+//    public static final Unsafe UNSAFE;
+//
+//    public static final List<Unsafe> UNSAFES = new LinkedList<>();
+//
+//    static {
+//        try {
+//            java.lang.reflect.Field f = LockSupport.class.getDeclaredField("UNSAFE");
+//            f.setAccessible(true);
+//            UNSAFE = (Unsafe) f.get(null);
+//        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+//            Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+//            System.err.println(ex);
+//            throw new RuntimeException();
+//        }
+//    }
     private Parent p;
 
     /**
@@ -61,24 +57,21 @@ public class Start extends Application {
     @Override
     public void init() {
         try {
-            for (int i = 0; i < 200; i++) {
-                try {
-                    UNSAFES.add((Unsafe) UNSAFE.allocateInstance(Unsafe.class));
-                } catch (InstantiationException ex) {
-                    Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-            super.notifyPreloader(new ProgressNotification(0));
+//            for (int i = 0; i < 200; i++) {
+//                try {
+//                    UNSAFES.add((Unsafe) UNSAFE.allocateInstance(Unsafe.class));
+//                } catch (InstantiationException ex) {
+//                    Logger.getLogger(Start.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//            }
             FXMLLoader loader = new FXMLLoader(Start.class.getResource("/itzfx/fxml/FXML.fxml"));
             p = loader.load();
             p.getStylesheets().add("/itzfx/fxml/Resources.css");
             fxml = loader.getController();
             fxml.inject(this);
             Thread current = Thread.currentThread();
-            super.notifyPreloader(new ProgressNotification(.3));
             PULSER.schedule(() -> LockSupport.unpark(current), 3, TimeUnit.SECONDS);
             LockSupport.park(this);
-            super.notifyPreloader(new ProgressNotification(1));
             PULSER.schedule(() -> LockSupport.unpark(current), 500, TimeUnit.MILLISECONDS);
             LockSupport.park(this);
         } catch (IOException ex) {
